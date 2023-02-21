1a4d18085a769f05377e1c8d5b0b7e5e70f827a0
guii-souza48 <guilherme-souza150694@hotmail.com>
2023-02-20 21:16:58 -0300

    Versao entrega

--- src/test/java/cenario1.java
+++ src/test/java/cenario1.java
@@ -1,7 +1,5 @@
 import com.sun.source.tree.TryTree;
-import org.openqa.selenium.By;
-import org.openqa.selenium.NoSuchElementException;
-import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.WebDriverWait;
@@ -16,19 +14,29 @@ public class cenario1 {
     public static void main(String[] args) {
         System.setProperty("webdriver.chrome.driver", "src\\drive_oficial\\chromedriver.exe");
         WebDriver driver = new ChromeDriver();
-        driver.manage().timeouts().implicitlyWait(0, TimeUnit.SECONDS);
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         driver.get("https://verity.com.br");
-        String text = driver.findElement(By.xpath("//*[@id=\"comp-kwv1yx4h\"]/h1/span")).getText();
 
-        if  (driver.getPageSource().contains("//*[@id=\"comp-kwv1yx4h\"]/h1/span"))
-        {
-            System.out.println("O texto pesquisado : " + text + " existe.");
-        } else {
-            System.out.println("O texto pesquisado : " + text + " existe.");
+        try{
+            WebElement textculture = driver.findElement(By.xpath("//*[text()='Culture e Tech']"));
 
-            driver.quit();
+            if(textculture.isDisplayed())
+            {
+                System.out.println("O texto pesquisado" + textculture.getText() + "não existe");
             }
+        }catch(Exception ex){
+            System.out.println("O texto pesquisado Culture e Tech não existe");
+        }
+        try{
+            WebElement textsolucoes = driver.findElement(By.xpath("//*[text()='Nossas soluções']"));
 
+            if(textsolucoes.isDisplayed())
+            {
+                System.out.println("O texto pesquisado " + textsolucoes.getText() + " existe");
+            }
+        }catch(Exception ex){
+            System.out.println("O texto pesquisado 'Nossas soluções' existe");
+        }
         }
     }
 
--- src/test/java/cenario2.java
+++ src/test/java/cenario2.java
@@ -2,51 +2,60 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-
-import static org.junit.Assert.assertEquals;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class cenario2 {
 
-    public static void main (String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         System.setProperty("webdriver.chrome.driver", "src\\drive_oficial\\chromedriver.exe");
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("start-maximized");
+        options.addArguments("disable-infobars");
+        options.addArguments("--disable-extensions");
         WebDriver driver = new ChromeDriver();
+
         driver.get("https://verity.com.br");
 
-
+        var waitTime = new Thread();
+        waitTime.sleep(5000);
         WebElement acessomenu = driver.findElement(By.id("comp-kevyodnk5label"));
         acessomenu.click();
-
+        waitTime.sleep(5000);
         driver.findElement(By.id("input_comp-kwz6tqej")).sendKeys("Guilherme Souza");
+        waitTime.sleep(5000);
         driver.findElement(By.id("input_comp-kwz6tqf7")).sendKeys("guilherme-souza16@hotmail.com");
+        waitTime.sleep(5000);
         driver.findElement(By.id("input_comp-kwz6tqfe")).sendKeys("963415887");
-        driver.findElement(By.id("textarea_comp-kwz6tqfr")).sendKeys("Não dominar uma habilidade necessária para a sua função;\n" +
-                "Lidar com a pressão e entregar algo com rapidez, mesmo que o tempo pareça insuficiente;\n" +
-                "Precisar atender a um imprevisto mesmo estando em uma tarefa prioritária;");
+        waitTime.sleep(5000);
+        driver.findElement(By.id("textarea_comp-kwz6tqfr")).sendKeys("""
+            Não dominar uma habilidade necessária para a sua função;
+            Lidar com a pressão e entregar algo com rapidez, mesmo que o tempo pareça insuficiente;
+            Precisar atender a um imprevisto mesmo estando em uma tarefa prioritária;""");
 
         //Validacao de valores
-        String nomeRetronado = driver.findElement(By.id("input_comp-kwz6tqej")).getAttribute("value");
-        String emailRetronado = driver.findElement(By.id("input_comp-kwz6tqf7")).getAttribute("value");
-        String telefoneRetronado = driver.findElement(By.id("input_comp-kwz6tqfe")).getAttribute("value");
-        String desafioRetronado = driver.findElement(By.id("textarea_comp-kwz6tqfr")).getAttribute("value");
+        String nomeRetornado = driver.findElement(By.id("input_comp-kwz6tqej")).getAttribute("value");
+        String emailRetornado = driver.findElement(By.id("input_comp-kwz6tqf7")).getAttribute("value");
+        String telefoneRetornado = driver.findElement(By.id("input_comp-kwz6tqfe")).getAttribute("value");
+        String desafioRetornado = driver.findElement(By.id("textarea_comp-kwz6tqfr")).getAttribute("value");
 
-        assertEquals("Guilherme Souza",nomeRetronado );
-        assertEquals("guilherme-souza16@hotmail.com",emailRetronado );
-        assertEquals("963415887", telefoneRetronado );
-        assertEquals("Não dominar uma habilidade necessária para a sua função;\n" +
-                "Lidar com a pressão e entregar algo com rapidez, mesmo que o tempo pareça insuficiente;\n" +
-                "Precisar atender a um imprevisto mesmo estando em uma tarefa prioritária;", desafioRetronado);
+        waitTime.sleep(5000);
 
-       WebElement chkBox= driver.findElement(By.className("cCdhN7"));
-       chkBox.click();
-       System.out.println(chkBox.isSelected());
+        WebElement chkBox = driver.findElement(By.className("cCdhN7"));
+        waitTime.sleep(5000);
+        chkBox.click();
 
-       driver.findElement(By.className("M3I7Z2")).click();
+        driver.findElement(By.className("M3I7Z2")).click();
+        waitTime.sleep(5000);
 
-       //driver.close();
+        WebDriverWait wait = new WebDriverWait(driver, 15);
+        wait.until(ExpectedConditions.frameToBeAvailableAndSwitchToIt(By.xpath("//iframe[starts-with(@name,'a-') and starts-with (@src, 'https://www.google.com/recaptcha')]")));
+        wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//div[@class =  'recaptcha-checkbox-border']"))).click();
+        driver.switchTo().defaultContent();
 
-
-
-
+        waitTime.sleep(30000);//tempo para resolver o captcha
+        driver.findElement(By.className("M3I7Z2")).click();
+        driver.close();
     }
 }
-
